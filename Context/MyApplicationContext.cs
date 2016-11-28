de7d5491a0e3aa05a2a2e1330f402ce676da15ed
Innerbytes <info@innerbytes.com>
2016-11-28 17:49:44 +0100

    Log extracted to the dll.

--- Context/MyApplicationContext.cs
+++ Context/MyApplicationContext.cs
@@ -5,8 +5,6 @@ using MinDI;
 namespace Custom.Demo.Context {
 	public class MyApplicationContext : IApplicationContextInitializer {
 		public void Initialize(IDIContext context) {
-			// context.s().Bind<ILog>(() => new SimpleLog());
-			context.s().Bind<ILog>(() => new DecoratedLog(new SimpleLog()));
 			context.s().Bind<IWorld>(() => new Earth());
 			context.s().Bind<ISky>(() => new Sky());
 			context.m().Bind<IHumanFactory>(() => new HumanFactory());
---
+++ LogSystem/Context/LogSystemContextInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Custom.Demo;
+using Custom.Demo.Implementations;
+using MinDI;
+
+namespace LogSystem
+{
+	public class LogSystemContextInitializer : IGlobalContextInitializer
+	{
+		public void Initialize(IDIContext context)
+		{
+			// context.s().Bind<ILog>(() => new SimpleLog());
+			context.s().Bind<ILog>(() => new DecoratedLog(new SimpleLog()));
+		}
+	}
+}
